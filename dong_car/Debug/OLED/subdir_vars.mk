4c9e05184c12a73e508684aa32475fe20b4032be
kaka52220 <2726651173@qq.com>
2026-05-19 10:25:43 +0800

    环境搭建测试

---
+++ dong_car/Debug/OLED/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../OLED/draw.c \
+../OLED/oled.c 
+
+C_DEPS += \
+./OLED/draw.d \
+./OLED/oled.d 
+
+OBJS += \
+./OLED/draw.o \
+./OLED/oled.o 
+
+OBJS__QUOTED += \
+"OLED\draw.o" \
+"OLED\oled.o" 
+
+C_DEPS__QUOTED += \
+"OLED\draw.d" \
+"OLED\oled.d" 
+
+C_SRCS__QUOTED += \
+"../OLED/draw.c" \
+"../OLED/oled.c" 
+
+
---
+++ dong_car/Debug/subdir_rules.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"G:/MSPM0 3507/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"G:/dong_car_code/dong_car" -I"G:/dong_car_code/dong_car/Debug" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+build-385756354: ../empty.syscfg
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: SysConfig'
+	"G:/MSPM0 3507/CCS/sysconfig_1.26.2/sysconfig_cli.bat" -s "G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/.metadata/product.json" --script "G:/dong_car_code/dong_car/empty.syscfg" -o "." --compiler ticlang
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+device_linker.cmd: build-385756354 ../empty.syscfg
+device.opt: build-385756354
+device.cmd.genlibs: build-385756354
+ti_msp_dl_config.c: build-385756354
+ti_msp_dl_config.h: build-385756354
+Event.dot: build-385756354
+
+%.o: ./%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"G:/MSPM0 3507/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"G:/dong_car_code/dong_car" -I"G:/dong_car_code/dong_car/Debug" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+startup_mspm0g350x_ticlang.o: G:/MSPM0\ 3507/CCS/mspm0_sdk_2_10_00_04/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"G:/MSPM0 3507/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.4.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"G:/dong_car_code/dong_car" -I"G:/dong_car_code/dong_car/Debug" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source/third_party/CMSIS/Core/Include" -I"G:/MSPM0 3507/CCS/mspm0_sdk_2_10_00_04/source" -gdwarf-3 -Wall -MMD -MP -MF"startup_mspm0g350x_ticlang.d_raw" -MT"startup_mspm0g350x_ticlang.o"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
